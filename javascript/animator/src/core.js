582d47e7294df7cc3356134d9b692a02fd271a89
eccyan <g00.eccyan@gmail.com>
2011-05-31 02:30:05 +0900

    Add show frame test

--- javascript/animator/src/core.js
+++ javascript/animator/src/core.js
@@ -22,6 +22,27 @@ var Animator = (function () {
 			}
 		    });
 		}
+	    },
+	    show : function () {
+		if (arguments.length == 1) {
+		    var frame = arguments[0];
+		    util.ajax({
+			url : url,
+			mimeType : 'text/plain; charset=x-user-defined',
+			success : function (xhr) {
+			    var bytes = util.stringToBytes(xhr.responseText);
+			    var image = new Gif(bytes);
+			    var imgElement = document.createElement('img');
+			    imgElement.src = 'data:image/gif;base64,'+util.encodeBase64([].concat(
+				image.getHeader().getData(),
+				image.getBlocks().get(1+frame).getData(),
+				image.getBlocks().get(1+frame+1).getData(),
+				[0x3b]
+			    ));
+			    element.appendChild(imgElement);
+			}
+		    });
+		}
 	    }
 	};
     };
