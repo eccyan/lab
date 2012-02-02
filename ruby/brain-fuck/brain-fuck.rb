b6c82912f16773fec936df6e1aaf8d744ca9611b
Daisuke Sato <g00.eccyan@gmail.com>
2012-02-02 19:44:10 +0900

    ochinchin

--- ruby/brain-fuck/brain-fuck.rb
+++ ruby/brain-fuck/brain-fuck.rb
@@ -1,34 +1,74 @@
+# -*- coding: utf-8 -*-
 class String
   def fuck
-    operations = {
-      '+' => lambda do |buffer, position|
-        buffer[position] = (buffer[position] + 1) & 0xff
-        [buffer, position]
-      end,
-      '-' => lambda do |buffer, position|
-        buffer[position] = (buffer[position] - 1) & 0xff 
-        [buffer, position]
-      end,
-      '>' => lambda do |buffer, position|
-        buffer[position + 1] ||= 0 
-        [buffer, position + 1]
-      end,
-      '<' => lambda do |buffer, position|
-        [buffer, [0, position - 1].max]
-      end,
-      '[' => lambda do |buffer, position|
-      end,
-      ']' => lambda do |buffer, position|
-      end,
-    }
-
-    buffer = [0]
-    position = 0
-    self.split // do |operation|
-      buffer, position = operations.fetch(operation).call buffer, position
+    identity = lambda do |value|
+      return value
+    end
+    increment = lambda do |value|
+      return value+1
+    end
+    decrement = lambda do |value|
+      return value-1
     end
 
-    buffer
+    increment_pc = lambda do |pc, memory, pointer|
+      return (increment.call pc), memory, pointer
+    end
+    decrement_pc = lambda do |pc, memory, pointer|
+      return [0, (decrement.call pc)].max, memory, pointer
+    end
+
+    increment_pointer = lambda do |pc, memory, pointer|
+      memory[increment.call pointer] ||= 0 
+      return increment_pc.call pc, memory, (increment.call pointer)
+    end
+    decrement_pointer = lambda do |pc, memory, pointer|
+      return increment_pc.call pc, memory, [0, (decrement.call pointer)].max
+    end
+    increment_value = lambda do |pc, memory, pointer|
+      memory[pointer] = (increment.call (memory.fetch pointer)) & 0xff
+      return increment_pc.call pc, memory, pointer
+    end
+    decrement_value = lambda do |pc, memory, pointer|
+      memory[pointer] = (decrement.call (memory.fetch pointer)) & 0xff
+      return increment_pc.call pc, memory, pointer
+    end
+
+    loop_out = lambda do |pc, memory, pointer|
+      find = lambda do |pc, nest|
+        return pc if nest.zero?
+        new_pc = increment.call pc
+        operation = {?[ => increment, ?] => decrement}.fetch (self.slice new_pc), identity
+        find.call new_pc, (operation.call nest)
+      end
+      return increment_pc.call pc, memory, pointer if (memory.at pointer).nonzero?
+      return find.call(pc, 1), memory, pointer
+    end
+    loop_in = lambda do |pc, memory, pointer|
+      find = lambda do |pc, nest|
+        return pc if nest.zero?
+        new_pc = decrement.call pc
+        operation = {?[ => decrement, ?] => increment}.fetch (self.slice new_pc), identity 
+        find.call new_pc, (operation.call nest)
+      end
+      return increment_pc.call pc, memory, pointer if (memory.at pointer).zero?
+      return find.call(pc, 1), memory, pointer
+    end
+    
+    operations = {
+      ?+ => increment_value, ?- => decrement_value,
+      ?> => increment_pointer, ?< => decrement_pointer,
+      ?[ => loop_out, ?] => loop_in,
+    }
+    pc = 0 #pragram counter
+    memory = [0]
+    pointer = 0
+    while self.size > pc 
+      pc, memory, pointer = (operations.fetch (self.slice pc), increment_pc)
+        .call pc, memory, pointer
+    end
+
+    memory
   end
 end
 
@@ -36,6 +76,8 @@ end
 '-'.fuck #=> [255]
 '>'.fuck #=> [0, 0]
 '<'.fuck #=> [0]
+'++コメント++'.fuck #=> [4]
+'+++[>+<-]'.fuck #=> [0, 3]
 ('+'*256).fuck #=> [0]
 '+>+'.fuck #=> [1, 1]
 '>><+'.fuck #=> [0, 1, 0]
